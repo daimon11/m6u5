425c53d7b838c2a9ec3d76ba6cfdae899037bab3
Dmitriy Bezgodko <daimon.94@mail.ru>
2023-01-02 22:51:02 +0300

    first

--- script/index.js
+++ script/index.js
@@ -1,42 +1,65 @@
-// import { numberPage } from './modules/getNumberPage.js';
-import { paginationControl } from './modules/pagination.js';
-import { renderGoods } from './modules/renderArticleList.js';
-import { renderArticle } from './modules/renderArticlePage.js'
+import fetchRequest from './modules/fetchRequest.js';
+import { renderGoods } from './modules/renderGoods.js';
+import { renderSearch } from './modules/renderGoods.js';
+import { renderFourNews } from './modules/renderGoods.js';
+import preload from './modules/preload.js';
 
-const renderArticles = (page) => {
+const news = document.querySelector('.new');
+const header = document.querySelector('.header');
+const footer = document.querySelector('.footer');
 
-  const btnPrev = document.querySelector('.footer__btn-prev');
-  const btnNext = document.querySelector('.footer__btn-next');
-  const pagesBtn = document.querySelectorAll('.item');
+const newsTopics = document.querySelector('.new');
 
-  paginationControl(btnPrev, btnNext, pagesBtn, page);
-  renderGoods(page);
-}
+const form = document.querySelector('.header__form');
+const searchTopic = document.querySelector('.search');
 
-let url = window.location.search;
-let searchParams = new URLSearchParams(url);
-if (searchParams.has("id")) {
-  let id = searchParams.get("id") ?
-    searchParams.get("id") : '';
-
-  const articleModal = document.querySelector('.article__wrapper');
-
-  renderArticle(    
-    id,
-    articleModal
-    );
-} else {
-  let page = searchParams.get("page") ?
-    searchParams.get("page") : 1;
-
-  let currentPage = page;
-
-  let currentActive = +currentPage;
-
-  renderArticles(currentActive);
+const initSearch = (data) => {
+  newsTopics.innerHTML = '';
+  return Promise.all([
+    fetchRequest(`everything?q=${data}`, {
+      callback: renderSearch,
+      headers: {
+        'X-Api-Key': 'e3348a594ddf4b52ba0ea024e1428837',
+      },
+    }),
+    fetchRequest('top-headlines?country=ru', {
+      callback: renderFourNews,
+      headers: {
+        'X-Api-Key': 'e3348a594ddf4b52ba0ea024e1428837',
+      },
+    })
+  ]);
 };
 
+const startPage = new Promise((resolve) => {
+  preload.show();
+  resolve(
+    fetchRequest('top-headlines?country=ru', {
+      callback: renderGoods,
+      headers: {
+        'X-Api-Key': 'e3348a594ddf4b52ba0ea024e1428837',
+      },
+    })
+  )
+});
 
+startPage.then(data => {
+  preload.remove();
+  header.classList.remove('visually-hidden');
+  news.classList.remove('visually-hidden');
+  footer.classList.remove('visually-hidden');
+  newsTopics.append(data);
+});
 
-
-
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const dataSearch = Object.fromEntries(formData);
+  console.log('dataSearch', dataSearch);
+  initSearch(dataSearch.body).then(item => {
+    searchTopic.innerHTML = '';
+    newsTopics.innerHTML = '';
+    searchTopic.append(item[0]);
+    newsTopics.append(item[1]);
+  });
+});
--- script/modules/uploadArticles.js
+++
@@ -1,5 +0,0 @@
-export const loadGoods = async (url) => {
-  const result = await fetch(url);
-  const data = await result.json();
-  return data.data;
-};
